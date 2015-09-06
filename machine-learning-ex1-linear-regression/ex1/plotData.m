4763b657cc4dea92ab305f965c4078fab9b567cf
Angel Ortega <an22ortega@gmail.com>
2015-09-05 19:07:42 -0700

    ao - correction in x and y axis for plotted data

--- machine-learning-ex1-linear-regression/ex1/plotData.m
+++ machine-learning-ex1-linear-regression/ex1/plotData.m
@@ -17,8 +17,8 @@ function plotData(x, y)
 figure; % open a new figure window
 
 plot(x,y,'rx','MarkerSize',10);
-xlabel('Population');
-ylabel('Profit');
+xlabel('Population for cities in 10,000s');
+ylabel('Profit in $10,000s');
 
 
 
