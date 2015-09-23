dfb71dd6b2a60be2d4f80351cccdb13568bbf6ef
Angel Ortega <an22ortega@gmail.com>
2015-09-22 23:19:35 -0700

    ao - adding comment about how the sigmoid function is used to calculate my hypothesis

--- machine-learning-ex2-logistic-regression/ex2/sigmoid.m
+++ machine-learning-ex2-logistic-regression/ex2/sigmoid.m
@@ -9,6 +9,7 @@ g = zeros(size(z));
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
+% hypthosis - h_theta(x) = g(theta'*x)
 g = 1/(1+e^(-z));
 
 
