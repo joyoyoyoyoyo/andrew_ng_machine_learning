fb8382583dd661333a2048c8284b196992a4ba88
Angel Ortega <an22ortega@gmail.com>
2015-09-27 19:40:04 -0700

    ao - sigmoid function works with vectors

--- machine-learning-ex2-logistic-regression/ex2/sigmoid.m
+++ machine-learning-ex2-logistic-regression/ex2/sigmoid.m
@@ -10,7 +10,15 @@ g = zeros(size(z));
 %               vector or scalar).
 
 % hypthosis - h_theta(x) = g(theta'*x)
-g = 1/(1+e^(-z));
+
+% For optimization, I should considering using an element wise power operator instead of a for loop
+if length(g) > 1
+	for i=1:length(z)
+		g(i) = 1/(1+e^(-z(i)));
+	end
+else
+	g = 1/(1+e^(-z));
+end
 % g needs to operate with vectors, scalars, or matrices
 
 % =============================================================
