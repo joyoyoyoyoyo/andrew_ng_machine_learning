cba64150e75b13b95d46e5c6e6f7652671345fde
Angel Ortega <an22ortega@gmail.com>
2015-10-04 01:29:21 -0700

    ao - cost function works with regularization

--- machine-learning-ex3-neural-networks-multi-classification/ex3/lrCostFunction.m
+++ machine-learning-ex3-neural-networks-multi-classification/ex3/lrCostFunction.m
@@ -35,9 +35,10 @@ grad = zeros(size(theta));
 %           temp(1) = 0;   % because we don't add anything for j = 0  
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
-
+temp = theta;
+temp(1)=0;
 hypothesis = sigmoid(X*theta)
-J = (1/m)*sum(abs(-y(:).*log(hypothesis)-(1-y(:)).*log(1-hypothesis)));
+J = (1/m)*sum(abs(-y(:).*log(hypothesis)-(1-y(:)).*log(1-hypothesis))) + (lambda/(2*m))*sum(temp.*temp);
 
 tempX = [ 1; X(:)];
 tempTheta = theta;
