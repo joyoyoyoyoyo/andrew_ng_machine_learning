6684d5fa220b0170fe41b2e5c5b0b10c17f234ad
Angel Ortega <an22ortega@gmail.com>
2015-10-04 18:41:39 -0700

    ao - prediction setup

--- machine-learning-ex3-neural-networks-multi-classification/ex3/predictOneVsAll.m
+++ machine-learning-ex3-neural-networks-multi-classification/ex3/predictOneVsAll.m
@@ -29,12 +29,8 @@ X = [ones(m, 1) X];
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
 %       
-
-
-
-
-
-
+[hypothesis] = sigmoid(X*all_theta');
+[v p] = max(hypothesis,[],2);
 
 % =========================================================================
 
