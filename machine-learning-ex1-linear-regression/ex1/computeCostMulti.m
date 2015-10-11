21bd90f6b214ecd144d197f18ccdf64e2826f416
Angel Ortega <an22ortega@gmail.com>
2015-10-10 19:17:54 -0700

    ao - computeCostMulti is complete

--- machine-learning-ex1-linear-regression/ex1/computeCostMulti.m
+++ machine-learning-ex1-linear-regression/ex1/computeCostMulti.m
@@ -12,10 +12,12 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
+for i=1:rows(X)
+	hypothesis = X(i,:)*theta;
+	J = J + (hypothesis-y(i))^2;
+end
 
-
-
-
+J = 1/(2*m)*J;
 
 % =========================================================================
 
