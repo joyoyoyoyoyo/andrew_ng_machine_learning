353fc380a2ef94cd882923f2a2c455ca85c3c72e
Angel Ortega <an22ortega@gmail.com>
2015-10-13 23:58:24 -0700

    ao - prototype gradient descent multi

--- machine-learning-ex1-linear-regression/ex1/gradientDescentMulti.m
+++ machine-learning-ex1-linear-regression/ex1/gradientDescentMulti.m
@@ -17,15 +17,11 @@ for iter = 1:num_iters
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
-
-
-
-
-
-
-
-
-
+	for i=1:m
+		hypothesis = X(i,:)*theta;
+		partialDeriv = sum((hypothesis - y(i)).*X(i,:));
+		theta = theta - (alpha*(1/m)).*partialDeriv;
+	end
 
     % ============================================================
 
