74098d8217cc6ace81c12f793c557e7553969a56
Angel Ortega <an22ortega@gmail.com>
2015-10-14 23:18:39 -0700

    ao - not yet

--- machine-learning-ex1-linear-regression/ex1/gradientDescentMulti.m
+++ machine-learning-ex1-linear-regression/ex1/gradientDescentMulti.m
@@ -16,11 +16,11 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-	for i=1:m
-		hypothesis = X(i,:)*theta;
-		partialDeriv = sum((hypothesis - y(i)).*X(i,:));
-		theta = theta - (alpha*(1/m)).*partialDeriv;
+		partialDeriv = 0;
+		hypothesis = X*theta;
+		partialDeriv =  (hypothesis-y)*X(:);
+	for j=1:length(theta);
+		theta(j) = theta(j) - (alpha*(1/m))*partialDeriv;
 	end
 
     % ============================================================
