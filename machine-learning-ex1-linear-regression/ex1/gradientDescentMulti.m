8d16b1396e69dd1c48f1f02d836ca44f8034cda7
Angel Ortega <an22ortega@gmail.com>
2015-10-17 23:25:36 -0700

    ao - completed gradient descent for multivariate linear regression

--- machine-learning-ex1-linear-regression/ex1/gradientDescentMulti.m
+++ machine-learning-ex1-linear-regression/ex1/gradientDescentMulti.m
@@ -16,11 +16,18 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-		partialDeriv = 0;
-		hypothesis = X*theta;
-		partialDeriv =  (hypothesis-y)*X(:);
 	for j=1:length(theta);
-		theta(j) = theta(j) - (alpha*(1/m))*partialDeriv;
+		if j==1
+			temp = theta;
+		end
+		J = 0;
+		for i=1:rows(y)
+			J = J + (X(i,:)*theta - y(i))*X(i,j);
+		end
+		temp(j) = theta(j) - (alpha*(1/m))*J;
+		if j == length(theta)
+			theta = temp;
+		end
 	end
 
     % ============================================================
