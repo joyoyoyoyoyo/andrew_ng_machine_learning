1ee54c75100a7a53724b1687324b8d4ef62cfd4f
Angel Ortega <an22ortega@gmail.com>
2015-10-21 22:16:01 -0700

    ao - completed feature normalization

--- machine-learning-ex1-linear-regression/ex1/featureNormalize.m
+++ machine-learning-ex1-linear-regression/ex1/featureNormalize.m
@@ -24,15 +24,14 @@ sigma = zeros(1, size(X, 2));
 %               each feature. 
 %
 % Hint: You might find the 'mean' and 'std' functions useful.
-%       
-
-
-
-
-
-
-
-
+%      
+mu = mean(X);
+sigma = std(X);
+for i=1:rows(X)
+  for j=1:columns(X)
+    X_norm(i,j) = (X(i,j)-mu(j))/sigma(j);
+  end
+end
 
 % ============================================================
 
